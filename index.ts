9daa00f09ba62e06225c1ca7958dde228f02daed
Usov_Artem <artemka063@gmail.com>
2023-11-29 15:08:15 +0400

    feat: prime number function

---
+++ index.ts
@@ -0,0 +1,16 @@
+function isPrime(num: number): boolean {
+  if (num <= 1) {
+    return false;
+  }
+  if (num === 2) {
+    return true;
+  }
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+isPrime(9)
